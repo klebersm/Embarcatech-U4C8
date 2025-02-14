50239cef57c96f7871326a147f74f70207a9b5e4
Kleber Marçal <kleber.sm@gmail.com>
2025-02-14 11:14:54 -0300

    Fix da área do display mais comentários

--- U4C8.c
+++ U4C8.c
@@ -5,15 +5,19 @@
 // define os slices que serão utilizados
 static uint sliceR;
 static uint sliceB;
+// Variável auxiliar, para habilitar ou desabilitar os slices
 bool enableSlices;
 
+// Alarmes para controle de debounce dos botões
 static alarm_id_t btn_a_alm;
 static alarm_id_t btn_b_alm;
 static alarm_id_t joy_btn_alm;
 
+// Variável para controle do display
 ssd1306_t ssd;
 
 void init_btns() {
+    // Cria máscara com os valores dos 3 botões para inicialização
     uint btnMask = (0x01 << BUTTON_A) | (0x01 << BUTTON_B) | (0x01 << JOYSTICK_BTN);
     gpio_init_mask(btnMask);
     gpio_set_dir_in_masked(btnMask);
@@ -21,6 +25,7 @@ void init_btns() {
     gpio_pull_up(BUTTON_B);
     gpio_pull_up(JOYSTICK_BTN);
 
+    // Inicializa as interrupções para os botão A e botão do Joystick
     gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &debounce);
     gpio_set_irq_enabled_with_callback(JOYSTICK_BTN, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &debounce);
 }
@@ -39,12 +44,14 @@ void init_leds() {
     sliceB = pwm_gpio_to_slice_num(LED_B);
 
     // Configura os slices com os valores calculados
+    // WRAP foi setado para 2048, que é a posição central do joystick
+    // para facilitar o cálculo dos levels
     pwm_set_clkdiv(sliceR, DIV);
     pwm_set_wrap(sliceR, WRAP);
     pwm_set_clkdiv(sliceB, DIV);
     pwm_set_wrap(sliceB, WRAP);
 
-    // Inicia os slices
+    // Inicia o programa com slices habilitados
     pwm_set_enabled(sliceR, true);
     pwm_set_enabled(sliceB, true);
     enableSlices = true;
@@ -67,13 +74,22 @@ void init_display() {
 }
 
 void init_joystick_adc() {
+    // Inicia o ADC
     adc_init();
 
+    // Configura os pinos do Joystick como ADC
     adc_gpio_init(JOYSTICK_X);
     adc_gpio_init(JOYSTICK_Y);
 }
 
 uint64_t handle_btn_a(alarm_id_t id, void *user_data) {
+    // Se os slices estão habilitados, zera os levels antes de desabilitar
+    // garantindo que o led sempre fique desligado
+    if(enableSlices) {
+        pwm_set_gpio_level(LED_R, 0);
+        pwm_set_gpio_level(LED_B, 0);
+    }
+    // Inverte o valor da variável para habilitar/desabilitar os slices
     enableSlices = !enableSlices;
 }
 
@@ -92,46 +108,64 @@ static void debounce(uint gpio, uint32_t events) {
     }
 }
 
+void update_display(uint16_t joy_x, uint16_t joy_y) {
+    // Limpa o display e renderiza o retangulo mais externo (borda fina)
+    ssd1306_fill(&ssd, false);
+    ssd1306_rect(&ssd, 3, 3, 122, 58, true, false);
+
+    // Se o led verde está acionado, renderiza outro retangulo por dentro, duplicando a borda
+    if(gpio_get(LED_G))
+        ssd1306_rect(&ssd, 4, 4, 120, 56, true, false);
+
+    // Calcula a posição do quadrado relativa à posição do joystick
+    // Sempre subtrai 4 para centralizar o qudrado
+    int left = (joy_y * WIDTH / 4096) - 4;
+    int top = ((4096 - joy_x) * HEIGHT / 4096) - 4;
+
+    // Evita travamento da placa por coordenada menor que zero
+    if(left < 0) left = 0;
+    if (top < 0) top = 0;
+
+    // Evita que o quadrado passe dos limites do display
+    if(left > WIDTH - 8) left = WIDTH - 8;
+    if(top > HEIGHT - 8) top = HEIGHT - 8; 
+
+    ssd1306_rect(&ssd, top, left, 8, 8, true, true);
+    ssd1306_send_data(&ssd); // Atualiza o display
+}
+
+void update_pwm(uint16_t joy_x, uint16_t joy_y) {
+    // Habilita ou desabilita os slices, de acordo com a variável auxiliar de controle
+    pwm_set_enabled(sliceR, enableSlices);
+    pwm_set_enabled(sliceB, enableSlices);
+    
+    // Como o PWM foi inicilizado com WRAP de 2048,
+    // basta calcular a "distância" da posição do joystick até 2048
+    // e, usando a função abs para obter o módulo, carregar o valor direto no level
+    pwm_set_gpio_level(LED_R, abs(2048 - joy_x));
+    pwm_set_gpio_level(LED_B, abs(2048 - joy_y));
+}
+
 int main()
 {
     stdio_init_all();
 
-    init_btns();
-    init_leds();
-    init_display();
-    init_joystick_adc();
-
-    // ssd1306_rect(&ssd, 3, 3, 122, 58, true, false); // Desenha um retângulo
-    // ssd1306_send_data(&ssd); // Atualiza o display
+    init_btns();            // Inicializa as configurações dos botóes
+    init_leds();            // Inicializa as configurações dos leds
+    init_display();         // Inicializa o display
+    init_joystick_adc();    // Inicializa o joystick
     
     while (true) {
-        printf("Hello, world!\n");
-
-        pwm_set_enabled(sliceR, enableSlices);
-        pwm_set_enabled(sliceB, enableSlices);
-
+        // Lê os valores do joystick
         adc_select_input(0);
         uint16_t joy_x = adc_read();
         adc_select_input(1);
         uint16_t joy_y = adc_read();
         
-        pwm_set_gpio_level(LED_R, abs(2048 - joy_x));
-        pwm_set_gpio_level(LED_B, abs(2048 - joy_y));
-        
-        ssd1306_fill(&ssd, false);
-        ssd1306_rect(&ssd, 3, 3, 122, 58, true, false);
-        if(gpio_get(LED_G))
-            ssd1306_rect(&ssd, 4, 4, 120, 56, true, false);
-
-        // // ssd'
-        int left = joy_y * WIDTH / 4096;
-        int top = (4096 - joy_x) * HEIGHT / 4096; //joy_y * (HEIGHT / 4096);
-
-        if(left > WIDTH - 6) left = WIDTH - 6;
-        if (top > HEIGHT - 10) top = HEIGHT - 10;
-
-        ssd1306_rect(&ssd, top, left, 8, 8, true, true);
-        ssd1306_send_data(&ssd); // Atualiza o display
+        // Atualiza o pwm de acordo com a posição do joystick
+        update_pwm(joy_x, joy_y);              
+        // Atualiza o display de acordo com a posição do joystick
+        update_display(joy_x, joy_y);
 
         sleep_ms(100);
     }
